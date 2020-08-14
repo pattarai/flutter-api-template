cfd24af1de23bd93300fea6568332a785b6ba43d
Rahuls-iMac <rahwinside@gmail.com>
2020-08-14 16:02:03 +0530

    remove unwanted code

--- lib/api/datetime_API.dart
+++ lib/api/datetime_API.dart
@@ -4,8 +4,8 @@ import 'package:mermobile/utils/network_util.dart';
 
 class DateTimeRestDataSource {
   NetworkUtil _netUtil = new NetworkUtil();
-  static final BASE_URL = "https://xstack.azurewebsites.net";
-  static final API_URL = BASE_URL + "/get-current-datetime-day.php";
+  static final BASE_URL = "https://www.xyz.com";
+  static final API_URL = BASE_URL + "/xyz.php";
 
   Future<dynamic> getServerClock() {
     return _netUtil.post(API_URL).then((dynamic res) {
--- lib/api/hod_consolidated_report_API.dart
+++ lib/api/hod_consolidated_report_API.dart
@@ -4,12 +4,12 @@ import 'package:mermobile/utils/network_util.dart';
 
 class ConsolidationFullReportRestDataSource {
   NetworkUtil _netUtil = new NetworkUtil();
-  static final BASE_URL = "https://xstack.azurewebsites.net";
-  static final LOGIN_URL = BASE_URL + "/mgmt-class-consolidation.php";
+  static final BASE_URL = "https://www.xyz.com";
+  static final API_URL = BASE_URL + "/xyz.php";
 
   Future<dynamic> fetch(String username, String auth_token, String start_date,
       String end_date, String department, String year) {
-    return _netUtil.post(LOGIN_URL, body: {
+    return _netUtil.post(API_URL, body: {
       "username": username,
       "auth_token": auth_token,
       "start_date": start_date,
